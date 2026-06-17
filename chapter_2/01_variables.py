07426e2481149684c1f92bbe625f17488dccf06e
dipaksinghmahar <dipaksinghmahar8@gmail.com>
2026-06-17 23:32:54 +0545

    Added Chapter 2 Python file

---
+++ chapter_2/01_variables.py
@@ -0,0 +1,9 @@
+# A variable is the name given to a memory location in a program
+a=20
+b=30
+c="dipak"
+d="mahar"
+sum=a+b
+e=c+d
+print("the sum value is :",sum)
+print("the sum value is :",e)
---
+++ chapter_2/02_datatypes.py
@@ -0,0 +1,16 @@
+a=2  #a is an integer
+b=2.222 # b is an float
+c="dipak" #c is an string
+d=False  #d is an boolen
+e=None #e is an none
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+# RULES FOR CHOOSING AN IDENTIFIER
+# • A variable name can contain alphabets, digits, and underscores.
+# • A variable name can only start with an alphabet and underscores.
+# • A variable name can’t start with a digit.
+# • No while space is allowed to be used inside a variable name.
