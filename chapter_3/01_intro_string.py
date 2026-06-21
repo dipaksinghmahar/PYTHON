5e7e617e934a77d394da77cf85aae453b4028469
dipaksinghmahar <dipaksinghmahar8@gmail.com>
2026-06-21 17:08:44 +0545

    Added Chapter 3 Python file

---
+++ chapter_3/01_intro_string.py
@@ -0,0 +1,23 @@
+a="dipak"
+b='dipaks'
+c='''dipaksingh'''
+
+print(a)
+print(b)
+print(c)
+
+#slicing
+d=a[0:2] #0 to 2 but 2 is not included
+print(d)
+ 
+e=c[0:9:2] #0 to 9 with step 2
+print(e)
+
+# negative indexing
+name = "dipak"
+print(name[-1])
+print(name[-5:-1])
+  
+#slicing the skip value
+x="0123456789"
+print(x[1:8:3]) #1 to 8 with step 3       
