85a03652d23ed859c9b4e972a383fb35cdd373f3
dipaksinghmahar <dipaksinghmahar8@gmail.com>
2026-07-23 21:23:33 +0545

    chapter_8

---
+++ chapter_8/01_function.py
@@ -0,0 +1,8 @@
+def avg():
+    a=int(input("enter the value of a :"))
+    b=int(input("enter the value of b :"))
+    c=int(input("enter the value of c :"))
+    d=(a+b+c)/3
+    print("the average of the value is :",d)
+
+avg()
